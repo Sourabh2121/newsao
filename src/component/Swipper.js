570be5d2a85573c04334ee19e66dd6950328e4ca
sourabh patil <ankitac@noitavonne.com>
2024-10-02 11:52:15 +0530

    added swiper

--- src/component/Swipper.js
+++ src/component/Swipper.js
@@ -1,45 +1,59 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+// Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css"; // Import Swiper styles
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/effect-coverflow";
 import "swiper/css/pagination";
-import "swiper/css/autoplay";
-import { Pagination, Autoplay } from "swiper/modules"; // Correct import from swiper/modules
 
-const ImageCarousel = () => {
-  const images = [
-    "/1.png",
-    "/2.png",
-    "/3.png",
-    "/4.png",
-    "/10.png",
-    "/11.png",
-    "/12.png",
-  ];
+// import required modules
+import { EffectCoverflow, Pagination } from "swiper/modules";
 
+export default function App() {
   return (
-    <div className="py-8">
+    <>
       <Swiper
-        modules={[Autoplay, Pagination]} // Register modules here
-        spaceBetween={60}
-        slidesPerView={6}
+        effect={"coverflow"}
+        grabCursor={true}
         centeredSlides={true}
-        autoplay={{ delay: 3000, disableOnInteraction: false }}
-        pagination={{ clickable: true }}
+        slidesPerView={"auto"}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        }}
+        pagination={true}
+        modules={[EffectCoverflow, Pagination]}
+        className="mySwiper"
       >
-        {images.map((src, index) => (
-          <SwiperSlide key={index}>
-            <img
-              src={src}
-              alt={`Slide ${index}`}
-              className={`carousel-image ${
-                index === Math.floor(images.length / 2) ? "zoom" : ""
-              }`} // Zoom effect on the middle slide
-            />
-          </SwiperSlide>
-        ))}
+        <SwiperSlide>
+          <img src="1.png" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="2.png" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="3.png" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="4.png" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="10.png" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="11.png" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="12.png" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="7.png" />
+        </SwiperSlide>
       </Swiper>
-    </div>
+    </>
   );
-};
-
-export default ImageCarousel;
+}
